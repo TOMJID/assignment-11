45dae69b549c49e018f472654198edbb3d2e9cc5
Tomjid huda <94817887+TOMJID@users.noreply.github.com>
2025-07-18 15:37:57 +0600

    nav section complete

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Avatar,
+} from "@heroui/react";
+
+function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Dashboard",
+    "Residents",
+    "Locations",
+    "Activities",
+    "Documents",
+    "Gather AI",
+  ];
+
+  return (
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      shouldHideOnScroll
+      isBordered
+      className="bg-white">
+      <NavbarContent className="md:hidden flex justify-between w-full">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
+        <NavbarBrand className="absolute left-1/2 transform -translate-x-1/2">
+          <img src="./Logo.svg" alt="logo " />
+        </NavbarBrand>
+        <div className="w-8" />
+      </NavbarContent>
+      <NavbarContent className="hidden md:flex">
+        <NavbarBrand>
+          <img src="./Logo.svg" alt="logo" />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent
+        className="hidden  md:flex gap-4 overflow-x-auto"
+        justify="center">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item}-${index}`} isActive={index === 0}>
+            <Link href="#" className="text-zinc-400 font-semibold">
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button isIconOnly variant="light" aria-label="Notifications">
+            <img src="./ball.svg" className="w-8" />
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Avatar
+            as="button"
+            className="transition-transform"
+            color="primary"
+            name="Usman Zafar"
+            size="sm"
+            src="./account.png"
+          />
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex flex-col text-zinc-400 -space-y-2">
+          <p className="text-sm ">Usman Zafar</p>
+          <p className="text-[10px]">usmanzafar@gmail.com</p>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link color="text-zinc-500" className="w-full" href="#" size="lg">
+                {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </Navbar>
+  );
+}
+
+export default Nav;
